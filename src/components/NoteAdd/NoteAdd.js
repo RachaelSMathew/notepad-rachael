0dcc42bdd4dba6e281310036054de310873ad622
Rachael Mathew <rachaelmathew1@gmail.com>
2023-02-03 20:02:35 -0500

    Update NoteAdd.js

--- src/components/NoteAdd/NoteAdd.js
+++ src/components/NoteAdd/NoteAdd.js
@@ -1,5 +1,5 @@
 // components/NoteAdd/NoteAdd.js
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import {useAuthValue} from '../../AuthContext'
 import firebase from "firebase/compat/app";
 import "firebase/compat/database";
@@ -8,7 +8,14 @@ import Switch from "react-switch";
 
 
 const NoteAdd = () => {
-  const [avatar, setAvatar] = React.useState(() => () => {
+  const [avatar, setAvatar] = React.useState([])
+
+  useEffect(()=>{
+    getAvatars()
+    console.log(avatar);//output 'sidebar'
+  },[])
+
+  const getAvatars = async () => {
     let results = []
     let exists = firebase
     .database()
@@ -17,27 +24,30 @@ const NoteAdd = () => {
       exists.orderByKey().once('value').then(function(snapshot)
         {
           snapshot.forEach(function(child) {
-            let avatar = child.val().email + " " + child.val().avatar
-            results.push(avatar);
-           })
-           
+            results.push(child.val());
+          })
+          
         }) 
     }
-    return results
-  });
-  
+    await delay(5000);
+    setAvatar(results)
+  };
+
   const emojis = ["😀", "😃", "😄", "😁", "😆", "😅", "🤣", "😂", "🙂", "🙃", "😉", "😊", "😇", "🥰", "😍", "🤩", "😘", "😗", "😚", "😙", "😋", "😛", "😝", "😜", "🤪", "🤨", "🧐", "🤓", "😎", "🤩", "🥳", "😏", "😒", "😞", "😔", "😟", "😕", "🙁", "☹️", "😣", "😖", "😫", "😩", "🥺", "😢", "😭", "😤", "😠", "😡", "🤬", "🤯", "😳", "🥵", "🥶", "😱", "😨", "😰", "😥", "😓", "🤗", "🤔", "🤭", "🤫", "🤥", "😶", "😐", "😑", "😬", "🙄", "😯", "😦", "😧", "😮", "😲", "😴", "🤤", "😪", "😵", "🤐", "🥴", "🤢", "🤮", "🤕", "🥳", "😷", "🤒", "🤕", "🤑", "🤠", "😈", "👿", "👹", "👺", "💀", "👻", "👽", "🤖", "💩", "😺", "😸", "😹", "😻", "😼", "😽", "🙀", "😿", "😾"]
   const {currentUser} = useAuthValue()
   const [checked, setChecked] = React.useState(false);
   const [height, setHeight] = React.useState(40);
 
-  const handleChange = () => {
+  const handleChange = async () => {
     setChecked(!checked);
   };
+  const delay = ms => new Promise(
+    resolve => setTimeout(resolve, ms)
+  );
   const avatarSet = () => {
-    const emojiExists = avatar.filter((ava) => ava.startsWith(currentUser?.email))
-    if(emojiExists[0]) {
-      return emojiExists[0].substring(emojiExists[0].indexOf(' ') + 1);
+    const emojiExists = avatar.find(ava => ava.includes(currentUser?.email))
+    if(emojiExists) {
+      return emojiExists.substring(emojiExists.indexOf(' ') + 1);
     }
     let emoji = emojis[Math.floor(Math.random() * emojis.length)]
     let avatarClone = avatar
@@ -59,7 +69,8 @@ const NoteAdd = () => {
     setDescription(event.target.value);
   };
 
-  const addNote = () => {
+  const addNote = async () => {
+    let avatarSign = avatarSet()
     if (title !== "" && description !== "") {
         if(checked) {
             firebase.database().ref("notebook/public").push({
@@ -67,7 +78,7 @@ const NoteAdd = () => {
                 email: currentUser?.email,
                 public: true,
                 description: description,
-                avatar: avatarSet(),
+                avatar: avatarSign,
                 heart: []
               });
         } else {
@@ -76,7 +87,7 @@ const NoteAdd = () => {
             email: currentUser?.email,
             public: false,
             description: description,
-            avatar: avatarSet(),
+            avatar: avatarSign,
             heart: []
         });
         }
@@ -93,7 +104,7 @@ const NoteAdd = () => {
             name="noteadd-header"
             placeholder="Note Title maxChar = 30"
             value={title}
-            maxlength = "30" 
+            maxLength = "30" 
             onChange={(val) => handleTitleChange(val)}
             ></textarea>
         </div>
@@ -108,7 +119,7 @@ const NoteAdd = () => {
         </div>
         
         <div className="noteadd-button">
-          <button onClick={() => addNote()}>Add a Note</button>
+          <button onClick={addNote}>Add a Note</button>
           <Switch checked={checked} onChange={handleChange} 
           handleDiameter={28}
           offColor="#08f"

f0d4ef7edde1ca83733457fd5296bf011bd7eb90
Rachael Mathew <rachaelmathew1@gmail.com>
2023-12-08 19:05:44 -0500

    Update Profile.js

--- src/Profile.js
+++ src/Profile.js
@@ -80,17 +80,3 @@ function Profile() {
 }
 
 export default Profile
-
-/*
-<div className='center'>
-        <div className='profile'>
-          <h1>Profile</h1>
-          <p><strong>Email: </strong>{currentUser?.email}</p>
-          <p>
-            <strong>Email verified: </strong>
-            {`${currentUser?.emailVerified}`}
-          </p>
-          <span onClick={() => signOut(auth)}>Sign Out</span>
-        </div>
-      </div>
-*/
